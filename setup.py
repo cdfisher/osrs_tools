e5f15d6d0f12b2bc5c9fcb12c5d2a14ec343a1ed
cdfisher <cdfisher96@gmail.com>
2023-05-29 12:51:02 -0400

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='osrs-tools',
+    version='0.1.1a2',
+    packages=['osrs-tools', 'osrs-tools.resources'],
+    url='https://github.com/cdfisher/osrs-tools',
+    license='',
+    author='Chris Fisher',
+    author_email='cdfisher96@gmail.com',
+    description='Package featuring a number of utilities for Old School RuneScape, including highscores integration, real-time Grand Exchange price information, and several useful calculators. '
+)
